604429016f51c9d6c80a380c5288cac266c0c008
hetame1 <poi8946@naver.com>
2023-07-03 21:24:15 +0900

    feat : 라우터추가, 기본작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { Routes, Route, Outlet } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage/'
+import Nav from './components/Nav'
 import './App.css'
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <>
+        <Nav />
+        
+        <Outlet />
+      </>
+    )
+  }
+
   return (
     <>
-      <div>
-        리팩토링
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Route>
+        </Routes>
       </div>
     </>
   )
