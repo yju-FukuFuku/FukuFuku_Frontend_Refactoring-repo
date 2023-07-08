0f76a28eb462e4d3e700131f95e692e22cd721fa
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-07-08 16:02:34 +0900

    Feat : 밝은 테마, 어두운 테마 추가했습니다
    - main.tsx : ThemeProvider 추가했습니다.
    - theme.ts : 밝은 테마, 어두운 테마 추가했습니다.

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor1: "#F8F9FA",
+    bgColor2: "#FFFFFF",
+    textColor1: "#212529",
+    textColor2: "#495057",
+    textColor3: "#868E96",
+    borderColor: "#F1F3F5",
+}
+
+export const darkTheme: DefaultTheme = {
+    bgColor1: "#121212",
+    bgColor2: "#1E1E1E",
+    textColor1: "#ECECEC",
+    textColor2: "#D9D9D9",
+    textColor3: "#ACACAC",
+    borderColor: "#2A2A2A",
+}
