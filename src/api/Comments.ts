e0c5d84d50795d28742fadb5678596c191e75876
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-07-30 02:50:13 +0900

    Feat : 댓글 api 추가
    - 댓글 CRUD api 추가 했습니다.

--- src/api/Comments.ts
+++ src/api/Comments.ts
@@ -1,5 +1,45 @@
 import axios from "axios";
 
+type Comment = {
+  content: string;
+  boardId: number;
+  commenter: string;
+  img: string;
+  u_id: number;
+}
+
+// 게시물의 댓글 가져오기
+export const getComment = async (boardId: number) => {
+  const { data } = await axios.get(`/comments/${boardId}`);
+  return data;
+}
+
+// 댓글 생성
+export const postComment = async (comment: Comment) => {
+  await axios.post('/comments', comment)
+    .then((res) => {
+      console.log(res.status)
+      const { data } = res;
+      return data;
+    }
+  )
+    .catch((error) => console.log(error)
+  );
+}
+
+// 댓글 삭제
+export const deleteComment = async (commentId: number) => {
+  await axios.delete(`/comments/${commentId}`)
+    .then((res) => console.log(res.status)
+  )
+}
+
+// 댓글 수정
+export const editComment = async (commentId:number, content: string) => {
+  await axios.patch(`/comments/${commentId}`, content)
+    .then
+}
+
 export async function getReply(c_id: number | null) {
   console.log(await axios.get(`/replys/${c_id}`));
   
