397d54a3d920c8a4dbd87a13ea706cef3ae478d4
hetame1 <poi8946@naver.com>
2023-09-17 01:36:36 +0900

    필요없는 파일 삭제, 모달 창 그냥 구글 로그인만 써서 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
+import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios';
 import { Provider } from 'react-redux'
 import { persistor, store } from './store'
@@ -12,42 +9,17 @@ import { persistor, store } from './store'
 axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.withCredentials = true
 
-import {
-  RecoilRoot,
-  useRecoilValue,
-} from 'recoil';
-import { themeSelector } from "./atom.ts";
 import { CookiesProvider } from 'react-cookie'
 import { PersistGate } from 'redux-persist/integration/react'
 
-const Root = () => {
-  // const [themeValue, setThemeValue] = useRecoilState(themeState);
-
-  // const theme = themeValue === true ? lightTheme : darkTheme;
-
-  const theme = useRecoilValue(themeSelector);
-
-  // const theme = themeType === "lightTheme" ? lightTheme : darkTheme;
-  
-  return (
-    <ThemeProvider theme={theme}>
-      <CookiesProvider>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </PersistGate>
-        </Provider>
-      </CookiesProvider>
-    </ThemeProvider>
-  );
-};
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-    <RecoilRoot>
-      <Root />
-    </RecoilRoot>
-  // </React.StrictMode>
+  <CookiesProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </CookiesProvider>
 )
