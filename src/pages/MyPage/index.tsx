dde23f7581f2e28458abcf04476f8a8d3a1251f6
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-10-01 00:58:18 +0900

    .

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -3,15 +3,12 @@ import style from './myPage.module.css'
 import useDebounce from '../../hooks/useDebounce';
 import Swal from 'sweetalert2'  // 경고창 라이브러리
 import { useSelector } from 'react-redux';
-<<<<<<< HEAD
-import { RootState } from '../../store';
-import { getCheck, deleteUser,editName } from '../../api/User';
-=======
 import { RootState, store } from '../../store';
 import api from '../../api';
 import { setUser } from '../../store/User';
 import { useNavigate } from 'react-router-dom';
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
+import { getCheck, deleteUser,editName } from '../../api/User';
+import { editUserImage } from '../../api/Image';
 
 const MyPage = () => {
   const navigate = useNavigate();
@@ -19,24 +16,18 @@ const MyPage = () => {
   const token = useSelector((state: RootState) => state.token);
   const userEmail = user.email || '불러오지 못했습니다.';
   const myHeader = new Headers();
-<<<<<<< HEAD
-  myHeader.append( 'Content-type', 'application/json' )
-  myHeader.append( 'Authorization', `Bearer ${token}` )
-
-  const [userId, setUserId] = useState<string>('');
-=======
   myHeader.append('Content-type', 'application/json')
   myHeader.append('Authorization', `Bearer ${token}`)
   
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
+  const [userId, setUserId] = useState<string>('');
   const [userName, setName] = useState<string>('');
   const [file, setFile] = useState<string>('')
   const [content, setContent] = useState<string | undefined>('')    // 한 줄 소개
   const [reName, setReName] = useState<boolean>(false)  // 닉네임 수정 check
   const fileInputRef = useRef<HTMLInputElement | null>(null); // 이미지 불러오기
   const [introCheck, setIntroCheck] = useState<boolean>(false)
-  
-  
+
+
   // 유저 정보 불러오기 (한 줄 소개, 닉네임, 이미지가 바뀔 떄마다)
   useEffect(() => {
     if (!user.isLogin) {
@@ -51,7 +42,7 @@ const MyPage = () => {
     setFile(user.picture ? user.picture : '');
     setContent(user.introduction || `${user.nickName} 입니다.`)
   }
-  
+
   const handleImageUpdate = () => { // 이미지 변경 요청
     console.log("이미지 변경")
     fileInputRef.current?.click();
@@ -87,6 +78,9 @@ const MyPage = () => {
     formData.append('file', e);
     myHeader.append('data', userEmail);
 
+    const editImage = editUserImage(formData)
+
+
     fetch("http://localhost:3000/user/editImage", {
       method: "PUT",
       headers: myHeader,
@@ -116,14 +110,14 @@ const MyPage = () => {
   // 닉네임 중복 체크 - debounce
   const [inputName, setInputName] = useState<string>(userName)
   const debounceVal = useDebounce(inputName, 300) // hook 불러오기
-
+  
   const handleInputName = (e: React.ChangeEvent<HTMLInputElement>) => {
     // 띄어쓰기 막기.
     if (!e.target.value.includes(" ")) {
       setInputName(e.target.value)
     }
   }
-
+  
   // debounceVal의 값이 변경되고 일정 시간이 지날때마다 함수 실행
   useEffect(() => {
     if (debounceVal != '' && debounceVal != userName) {
@@ -134,7 +128,6 @@ const MyPage = () => {
   // 닉네임 중복 체크 - Get  
   const handleNameOverlap = () => {
     console.log(debounceVal)
-<<<<<<< HEAD
     getCheck(debounceVal)
       .then((response) => response.json())
       .then((data) => {
@@ -144,19 +137,6 @@ const MyPage = () => {
         } else if(data.statusCode == "409") {
           console.log(data.message)
         } else{
-=======
-    fetch(`http://localhost:3000/user/check/${debounceVal}`, {
-      headers: myHeader,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        if (data.statusCode == "200") {
-          console.log(data.message)
-        } else if (data.statusCode == "409") {
-          console.log(data.message)
-        } else {
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
           console.log("정의되지 않은 오류입니다.")
         }
       })
@@ -167,7 +147,6 @@ const MyPage = () => {
   // 닉네임 중복체크 후 닉네임 수정 - Put
   const handleNameUpdate = () => {
     console.log("이름 변경")
-<<<<<<< HEAD
     const nameObj = {
       data: {
         where: { id: 1 },
@@ -177,40 +156,12 @@ const MyPage = () => {
 
     // api 요청
     editName(nameObj)
-=======
-    fetch("http://localhost:3000/user/editNickname", {
-      method: "PUT",
-      headers: myHeader,
-      body: JSON.stringify({
-        "data": {
-          "where": { "email": "9000248@g.yju.ac.kr" },  // 바꾸려는 유저의 이메일
-          "data": { "nickName": "test" } // 바꾸려는 닉네임 값
-        }
-      })
-    })
-      .then((Response) => Response.json())
-      .then((data) => {
-        console.log(data)
-        if (data.statusCode == "200") {
-          console.log(data.message)
-          // setName(data.nickName)
-          // setReName(false)
-          console.log("변경완료")
-        } else if (data.statusCode == "400") {
-          console.log(data.message)
-        } else if (data.statusCode == "409") {
-          console.log(data.message)
-        }
-      })
-      .catch((error) => console.log(error))
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
     setReName(false)
   }
 
   // 회원탈퇴 fetch요청
   const handleUserRemove = () => {
     console.log("회원 탈퇴")
-<<<<<<< HEAD
     const deleteObj = {
       data: {
         where: {
@@ -218,32 +169,7 @@ const MyPage = () => {
         }
       }
     }
-
     deleteUser(deleteObj)
-=======
-    fetch("http://localhost:3000/user/withdraw", {
-      method: "DELETE",
-      headers: myHeader,
-      body: JSON.stringify({
-        "data": {
-          "where": { "id": userEmail }
-        }
-      })
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        if (data.statusCode == "201") {
-          console.log(data.message)
-          console.log("탈퇴 성공")
-        } else if (data.statusCode == "400") {
-          console.log(data.message)
-        } else {
-          console.log("정의되지 않은 오류입니다.")
-        }
-      })
-      .catch((error) => console.log(error))
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
   }
 
   // INTRO
@@ -259,20 +185,7 @@ const MyPage = () => {
     setContent(userData);
   }
 
-  // 서버로 데이터 전송
-<<<<<<< HEAD
-  const handleUpdateContent = () => { 
-    const introContent = {
-      updateData : { email: userEmail },
-      data : { introduction: content }
-    }
-
-    // fetch 요청
-    // introChange(introContent)
-    // setContent(userData)
-
     console.log("수정 완료")
-=======
   const handleUpdateContent = () => {
     api.patch("/user/editIntroduction", {
       data: {
@@ -294,7 +207,6 @@ const MyPage = () => {
         window.alert("문제가 생겼습니다. 나중에 다시 시도해주세요.");
         setContent(user.introduction || `${user.nickName} 입니다.`);
       })
->>>>>>> 88f521653fa82880e5e8c002c9d80ff82152a41c
     setIntroCheck(false)
   }
 
