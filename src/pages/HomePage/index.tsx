faac289ce3b7eb37c3f8405d486b11845e09ff1d
Z00One <9000248@g.yju.ac.kr>
2023-10-14 15:19:19 +0900

    refactor: Refactor request to include ID of last retrieved post

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,17 +1,13 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
-import { Link } from 'react-router-dom'
-import { styled } from 'styled-components'
-import Board from '../../components/board'
-import { getBoard } from '../../api/BoardAPI'
-import { PostType } from '../../components/board'
-import { debounce } from '@mui/material'
+import { useState, useEffect, useRef, useCallback } from "react";
+import { styled } from "styled-components";
+import Board from "../../components/board";
+import { getBoard } from "../../api/BoardAPI";
+import { PostType } from "../../components/board";
+import { debounce } from "@mui/material";
 
 const MainPage = () => {
-
-  
-
   const [posts, setPosts] = useState<PostType[]>([]);
-  const [page, setPage] = useState<number>(0);
+  const [lastId, setLastId] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [hasMore, setHasMore] = useState<boolean>(true);
   const [request, setRequest] = useState<boolean>(false);
@@ -19,95 +15,90 @@ const MainPage = () => {
 
   // 데이터 렌더링
   useEffect(() => {
-    loadMoreData()
-  }, [])
+    loadMoreData();
+  }, []);
 
   useEffect(() => {
     if (request && hasMore) {
-      loadMoreData()
+      loadMoreData();
     }
-  }, [request])
-  
+  }, [request]);
+
   // 데이터 불러오기
   const loadMoreData = useCallback(async () => {
-    if( loading || !hasMore) return;
+    if (loading || !hasMore) return;
     setLoading(true);
     try {
-      const newData = await getBoard(page, 6);
-      console.log(newData)
+      const newData = await getBoard(lastId, 6);
+      console.log(newData);
       if (JSON.stringify(posts) === JSON.stringify(newData)) {
         console.log("같음");
         return;
       }
       setPosts((prev) => [...prev, ...newData]);
-      console.log("new Data");
-      console.log(posts);
-      
-      if (newData.length <= 9){
+      // console.log("new Data");
+      // console.log(posts);
+
+      if (newData.length <= 9) {
         setHasMore(false);
-        console.log("more end");
-        
+        // console.log("more end");
       }
-      setPage(prev => prev + 1);
-      console.log("page up");
-      
-    } catch(error) {
-      console.error('Error loading more data: ', error);
+      const newLastId = newData[newData.length - 1]?.id;
+      setLastId(newLastId);
+      // console.log("lastId up");
+    } catch (error) {
+      console.error("Error loading more data: ", error);
     } finally {
       setLoading(false);
       setRequest(false);
     }
-  }, [loading, hasMore, page, posts])
-  
+  }, [loading, hasMore, lastId, posts]);
+
   // 스크롤
   useEffect(() => {
-      // 스크롤 체크  
+    // 스크롤 체크
     const container = containerRef.current;
-    if(!container) return;
-    
+    if (!container) return;
+
     const handleScroll = () => {
-      console.log("scrollHeight  " + container.scrollHeight);
-      console.log("clientHeight  " + container.clientHeight);
-      console.log("scrollTop  " + window.scrollY);
-      
-      if (container.scrollHeight - container.clientHeight + 100 <= window.scrollY) {
-        console.log("loadMore");
+      // console.log("scrollHeight  " + container.scrollHeight);
+      // console.log("clientHeight  " + container.clientHeight);
+      // console.log("scrollTop  " + window.scrollY);
+
+      if (
+        container.scrollHeight - container.clientHeight + 100 <=
+        window.scrollY
+      ) {
+        // console.log("loadMore");
         setRequest(true);
       }
     };
-    window.addEventListener('scroll', debounce(handleScroll));
+    window.addEventListener("scroll", debounce(handleScroll));
 
     return () => {
-      window.removeEventListener('scroll', debounce(handleScroll));
+      window.removeEventListener("scroll", debounce(handleScroll));
       setRequest(false);
     };
-  }, [])
+  }, []);
 
   useEffect(() => {
-    console.log("page 변경");
-    console.log(page);
-  }, [page])
+    // console.log("lastId 변경");
+    console.log(`lastId: ${lastId}`);
+  }, [lastId]);
 
   return (
     <>
-        {/* <div>MainPage
+      {/* <div>MainPage
           <Link to='/login'>Login</Link>
         </div> */}
-        <Container ref={containerRef}>
-          {
-            posts ? (
-              <Board posts={posts} />
-              ) : (
-                "none"
-                )
-              }
-        </Container>
+      <Container ref={containerRef}>
+        {posts ? <Board posts={posts} /> : "none"}
+      </Container>
     </>
+  );
+};
 
-  )
-}
-
-export default MainPage
+export default MainPage;
 
 const Container = styled.div`
   display: flex;
@@ -115,7 +106,7 @@ const Container = styled.div`
   padding: 0 2vw;
   /* overflow: scroll; */
 
-  @media (max-width: 767px){
+  @media (max-width: 767px) {
     padding: 0;
   }
-`
+`;
